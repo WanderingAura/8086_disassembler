bb68e5cd504bbe902554ebdf8c58c00fbe50e344
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2025-01-15 18:38:38 +0000

    [add] xchg/in/out asm

---
+++ tests/asm/xchg_in_out.asm
@@ -0,0 +1,19 @@
+xchg ax, [bp - 1000]
+xchg [bx + 50], bp
+
+xchg ax, ax
+xchg ax, dx
+xchg ax, sp
+xchg ax, si
+xchg ax, di
+
+xchg cx, dx
+xchg si, cx
+xchg cl, ah
+
+in al, 200
+in al, dx
+in ax, dx
+
+out 44, ax
+out dx, al
